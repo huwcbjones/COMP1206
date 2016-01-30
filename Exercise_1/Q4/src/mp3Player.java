58ab40fe0ddd200327319eef16174031fad42446
Huw Jones <huwcbjones@gmail.com>
2016-01-30 19:11:03 +0000

    Added mp3player

---
+++ Exercise_1/Q4/src/mp3Player.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+/**
+ * An mp3 Player Layout
+ *
+ * @author Huw Jones
+ * @since 30/01/2016
+ */
+public class mp3Player {
+
+    private JFrame window;
+    private JPanel panel_artwork;
+    private JPanel panel_controls;
+    private JPanel panel_playlist;
+
+    private JSlider slider_time;
+    private JButton btn_previous;
+    private JButton btn_playPause;
+    private JButton btn_next;
+    private JButton btn_stop;
+}
