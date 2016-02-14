1eaf331bdb39f7896452b3cb1e933f19b4d372e6
Huw Jones <huwcbjones@gmail.com>
2016-02-14 10:43:42 +0000

    Added base classes

---
+++ Exercise_3/Q2/Circle.java
@@ -0,0 +1,8 @@
+/**
+ * A Circle
+ *
+ * @author Huw Jones
+ * @since 14/02/2016
+ */
+public class Circle extends Shape {
+}
---
+++ Exercise_3/Q2/Shape.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * An abstract shape
+ *
+ * @author Huw Jones
+ * @since 14/02/2016
+ */
+public abstract class Shape extends JPanel {
+
+}
