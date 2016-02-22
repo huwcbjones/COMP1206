01c249f96d12fa5ff20f0cedd79289836db29999
Huw Jones <huwcbjones@gmail.com>
2016-02-22 15:24:34 +0000

    Added spinner controls to set iterations

--- Exercise_4/Q2/CircleRecursion.java
+++ Exercise_4/Q2/CircleRecursion.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Circle Recursion Application
@@ -8,28 +11,61 @@ import javax.swing.*;
  */
 public class CircleRecursion extends JFrame {
 
-    public CircleRecursion(int iterations) {
+    private CirclePanel panel_circle;
+    private JPanel panel_controls;
+    private JSpinner spinner_iterations;
+    private JLabel label_iterations;
+    private JButton btn_update;
+
+    public CircleRecursion() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+
+        }
+
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        CirclePanel panel = new CirclePanel(iterations);
+        panel_circle = new CirclePanel();
 
-        this.getContentPane().add(panel);
+        Container c = this.getContentPane();
+        c.setLayout(new BorderLayout());
+        c.add(panel_circle, BorderLayout.CENTER);
+
+        panel_controls = new JPanel(new FlowLayout());
+
+        label_iterations = new JLabel("Iterations: ");
+        panel_controls.add(label_iterations);
+
+        SpinnerNumberModel slm_iterations = new SpinnerNumberModel(3, 1, 20, 1);
+        spinner_iterations = new JSpinner(slm_iterations);
+        panel_controls.add(spinner_iterations);
+
+        btn_update = new JButton("Update");
+        btn_update.addActionListener(new updateHandler());
+        panel_controls.add(btn_update);
+
+        c.add(panel_controls, BorderLayout.PAGE_END);
 
         this.setVisible(true);
         this.repaint();
     }
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Please specify a number of iterations.");
-            return;
-        }
-        try {
-            int iterations = Integer.parseInt(args[0]);
-            CircleRecursion circle = new CircleRecursion(iterations);
-        } catch (NumberFormatException ex) {
-            System.err.println("Please specify a valid number of iterations.");
+        CircleRecursion circle = new CircleRecursion();
+    }
+
+    protected class updateHandler implements ActionListener {
+
+        /**
+         * Invoked when an action occurs.
+         *
+         * @param e
+         */
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            panel_circle.repaint((int) spinner_iterations.getValue());
         }
     }
 }
