31d516f9bb80a51b2c98437047f8676a2a587861
Huw Jones <huwcbjones@gmail.com>
2016-02-22 19:25:15 +0000

    Added Mandelbrot class

---
+++ Assignment_1/src/Mandelbrot.java
@@ -0,0 +1,21 @@
+import mandelbrot.Main;
+
+import javax.swing.*;
+
+/**
+ * Bootstrapper for application
+ *
+ * @author Huw Jones
+ * @since 22/02/2016
+ */
+public class Mandelbrot {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Main gui = new Main();
+            }
+        });
+    }
+
+}
