85d0f57cd0f6964d1ce5f20e21601d7b26036f43
Huw Jones <huwcbjones@gmail.com>
2016-02-27 15:02:50 +0000

    Started sorting layout out

--- Assignment_1/src/mandelbrot/Main.java
+++ Assignment_1/src/mandelbrot/Main.java
@@ -1,5 +1,8 @@
 package mandelbrot;
 
+
+import utils.SpringUtilities;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -11,17 +14,25 @@ import java.awt.*;
  */
 public class Main extends JFrame {
 
-    private JPanel panel_controls;
+    private static final double DISPLAY_CONSTRAINT = 0.75;
     private JPanel panel_display;
+    private JPanel panel_info;
+    private JPanel panel_julia;
+    private JPanel panel_controls;
 
+    // Axis Range
     private JLabel label_rangeX;
-    private JSpinner spinner_rangeX;
+    private JSpinner spinner_rangeMinX;
+    private JSpinner spinner_rangeMaxX;
     private JLabel label_rangeY;
-    private JSpinner spinner_rangeY;
+    private JSpinner spinner_rangeMinY;
+    private JSpinner spinner_rangeMaxY;
+
+    // Info
+    private JLabel label_complexPoint;
 
     public Main() {
         super("Mandelbrot Viewer");
-        this.getContentPane().setLayout(new BorderLayout());
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setMinimumSize(new Dimension(800, 600));
         try {
@@ -36,18 +47,74 @@ public class Main extends JFrame {
 
     private void initComponents(){
         Container c = this.getContentPane();
+        c.setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.BOTH;
 
         panel_display = new JPanel();
-        c.add(panel_display, BorderLayout.CENTER);
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.weightx = DISPLAY_CONSTRAINT;
+        constraints.weighty = DISPLAY_CONSTRAINT;
+        constraints.gridheight = 3;
+        panel_display.setBackground(Color.black);
+        c.add(panel_display, constraints);
+
+        initSidePanels(constraints);
+        initControlPanel();
+        initInfoPanel();
+    }
+
+    private void initSidePanels(GridBagConstraints c) {
+        Container pane = this.getContentPane();
+        c.weightx = 1 - DISPLAY_CONSTRAINT;
+        c.gridx = 1;
+        c.gridheight = 1;
+
+        panel_info = new JPanel();
+        c.gridy = 0;
+        c.weighty = 0.25;
+        pane.add(panel_info, c);
 
         panel_controls = new JPanel(new SpringLayout());
+        c.gridy = 1;
+        c.weighty = 0.25;
+        pane.add(panel_controls, c);
 
-        label_rangeX = new JLabel("X Range:");
+        panel_julia = new JPanel();
+        c.gridy = 2;
+        c.weighty = 0.5;
+        pane.add(panel_julia, c);
+    }
+
+    private void initControlPanel() {
+        label_rangeX = new JLabel("X Range:", JLabel.TRAILING);
         panel_controls.add(label_rangeX);
 
-        spinner_rangeX = new JSpinner(new SpinnerNumberModel())
+        spinner_rangeMinX = new JSpinner(new SpinnerNumberModel(-2, -10, 10, 0.2));
+        panel_controls.add(spinner_rangeMinX);
 
+        spinner_rangeMaxX = new JSpinner(new SpinnerNumberModel(2, -10, 10, 0.2));
+        panel_controls.add(spinner_rangeMaxX);
 
-        c.add(panel_controls, BorderLayout.LINE_START);
+        label_rangeY = new JLabel("Y Range:", JLabel.TRAILING);
+        panel_controls.add(label_rangeY);
+
+        spinner_rangeMinY = new JSpinner(new SpinnerNumberModel(-1.6, -10, 10, 0.2));
+        panel_controls.add(spinner_rangeMinY);
+
+        spinner_rangeMaxY = new JSpinner(new SpinnerNumberModel(1.6, -10, 10, 0.2));
+        panel_controls.add(spinner_rangeMaxY);
+
+        SpringUtilities.makeCompactGrid(panel_controls,
+                2, 3,
+                6, 6,
+                6, 6
+
+        );
+    }
+
+    private void initInfoPanel() {
+
     }
 }
