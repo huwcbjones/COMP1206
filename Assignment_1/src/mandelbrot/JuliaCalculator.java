3023c259f03bba9a1b6651428462df8b9f3af9da
Huw Jones <huwcbjones@gmail.com>
2016-02-28 14:02:36 +0000

    Added Julia/Mandelbrot Tasks

---
+++ Assignment_1/src/mandelbrot/JuliaCalculator.java
@@ -0,0 +1,44 @@
+package mandelbrot;
+
+import utils.ColouredPixel;
+import utils.Complex;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * {DESCRIPTION}
+ *
+ * @author Huw Jones
+ * @since 28/02/2016
+ */
+public class JuliaCalculator extends DrawingTask {
+    Complex complex;
+    public JuliaCalculator(DrawingManagementThread t, Rectangle2D bounds, int maxIterations, Complex c) {
+        super(t, bounds, maxIterations);
+        complex = c;
+    }
+
+    @Override
+    protected ColouredPixel doPixelCalculation(Point2D point, Complex c) {
+        int currIteration = 0;
+        Complex z = new Complex(c.getReal(), c.getImaginary());
+
+        while (z.modulusSquared() <= 4 && currIteration < maxIterations) {
+            z = z.square();
+            z.add(c);
+            currIteration++;
+        }
+
+        Color colour;
+
+        if (currIteration < maxIterations) {
+            colour = Color.getHSBColor(currIteration / 100f, 1, 1);
+        } else {
+            colour = Color.BLACK;
+        }
+
+        return new ColouredPixel(point, colour);
+    }
+}
