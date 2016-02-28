e3bd80a0a7665d1ccb90eedfd9e09ae6ac432414
Huw Jones <huwcbjones@gmail.com>
2016-02-28 02:16:53 +0000

    Implemented ImageCaching

--- Assignment_1/src/mandelbrot/DiversionCalculator.java
+++ Assignment_1/src/mandelbrot/DiversionCalculator.java
@@ -1,10 +1,13 @@
 package mandelbrot;
 
 import utils.Complex;
+import utils.ImageSegment;
 
 import java.awt.*;
-import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.Callable;
 
 /**
  * {DESCRIPTION}
@@ -12,26 +15,36 @@ import java.awt.geom.Point2D;
  * @author Huw Jones
  * @since 27/02/2016
  */
-public class DiversionCalculator implements Runnable {
+public class DiversionCalculator implements Callable<ImageSegment> {
 
     private DrawingThread drawingThread;
-    private Line2D bounds;
+    private Rectangle2D bounds;
     private int maxIterations;
+    private BufferedImage image;
 
-    public DiversionCalculator(DrawingThread t, Line2D bounds, int maxIterations) {
+    public DiversionCalculator(DrawingThread t, Rectangle2D bounds, int maxIterations) {
         this.drawingThread = t;
         this.bounds = bounds;
         this.maxIterations = maxIterations;
     }
 
+    /**
+     * Computes a result, or throws an exception if unable to do so.
+     *
+     * @return computed result
+     * @throws Exception if unable to compute a result
+     */
     @Override
-    public void run() {
+    public ImageSegment call() throws Exception {
+        image = new BufferedImage((int) bounds.getWidth(), (int) bounds.getHeight(), BufferedImage.TYPE_INT_RGB);
 
+        for (int y = 0; y < bounds.getHeight(); y++) {
+            for (int x = 0; x < bounds.getWidth(); x++) {
 
-        for(int y = (int)bounds.getY1(); y < bounds.getY2(); y++) {
-            for(int x = (int)bounds.getX1(); x < bounds.getX2(); x++) {
-                Point2D p = new Point2D.Float(x, y);
-                Complex c = drawingThread.getComplexFromPoint(p);
+                Point2D absP = new Point2D.Double(x + bounds.getX(), y + bounds.getY());
+                Point2D relP = new Point2D.Double(x, y);
+
+                Complex c = drawingThread.getComplexFromPoint(absP);
                 int currIteration = 0;
                 Complex z = new Complex(c.getReal(), c.getImaginary());
 
@@ -41,11 +54,19 @@ public class DiversionCalculator implements Runnable {
                     currIteration++;
                 }
                 if (currIteration < maxIterations) {
-                    drawingThread.paintPixel(p, Color.WHITE);
+                    paintPixel(relP, Color.getHSBColor(currIteration / 100f, 1, 1));
                 } else {
-                    drawingThread.paintPixel(p, Color.BLACK);
+                    paintPixel(relP, Color.BLACK);
                 }
             }
         }
+        return new ImageSegment(image, bounds);
+    }
+
+    private void paintPixel(Point2D p, Color c) {
+        int x = Double.valueOf(p.getX()).intValue();
+        int y = Double.valueOf(p.getY()).intValue();
+
+        this.image.setRGB(x, y, c.getRGB());
     }
 }
--- Assignment_1/src/mandelbrot/DrawingThread.java
+++ Assignment_1/src/mandelbrot/DrawingThread.java
@@ -1,14 +1,16 @@
 package mandelbrot;
 
 import utils.Complex;
+import utils.ImageCache;
 import utils.ImagePanel;
+import utils.ImageSegment;
 
 import java.awt.*;
-import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.LinkedHashMap;
+import java.util.concurrent.*;
 
 /**
  * Manages and delegates drawing/calculation threads
@@ -23,6 +25,7 @@ class DrawingThread extends Thread {
 
     private boolean hasDrawn = false;
 
+    private final Object shouldRedraw = new Object();
     double xShift;
     double yShift;
     double scaleFactor;
@@ -32,24 +35,117 @@ class DrawingThread extends Thread {
 
     double xScale;
     double yScale;
-
+    int iterations;
+    ExecutorCompletionService<ImageSegment> executorService;
     int numberThreads;
+    private LinkedHashMap<ImageCache, BufferedImage> images;
 
     public DrawingThread(Main mainWindow, ImagePanel panel) {
         this.mainWindow = mainWindow;
         this.panel = panel;
         this.setName("Mandelbrot_Drawing_Thread");
 
+        images = new LinkedHashMap<>();
+
         numberThreads = Runtime.getRuntime().availableProcessors();
+        // Get an executor service for the amount of cores we have
+        ExecutorService executorService = Executors.newFixedThreadPool(numberThreads);
+        this.executorService = new ExecutorCompletionService<>(executorService);
     }
 
     @Override
     public void run() {
+        while (!this.isInterrupted()) {
+            synchronized (this.shouldRedraw) {
+                try {
+                    shouldRedraw.wait();
+                    redraw();
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * Private method to do the redraw
+     */
+    private void redraw() {
+        if (!checkCacheValidity()) {
+            images = new LinkedHashMap<>();
+        }
+        calculateVariables();
+
+        if (checkCache()) {
+            return;
+        }
+
+
+        int numberStrips = numberThreads;
+        int stripWidth = (int) Math.floor(imgWidth / (numberStrips));
+
+        Rectangle2D bounds;
+
+        int start;
+        // Queue up strips to be calculated
+        for (int i = 0; i < numberStrips; i++) {
+            if (i == 0) {
+                start = 0;
+            } else {
+                start = i * stripWidth - 1;
+            }
+
+            bounds = new Rectangle2D.Double(start + 1, 0, stripWidth + 2, imgHeight);
+            executorService.submit(new DiversionCalculator(this, bounds, iterations));
+        }
+
+        Future<ImageSegment> result;
+        ImageSegment imgSeg;
+        Graphics2D g = (Graphics2D) image.getGraphics();
+
+        // Get results from strips
+        for (int i = 0; i < numberStrips; i++) {
+            try {
+                result = executorService.take();
+                if (!result.isDone()) {
+                    System.err.println("Failed to calculate segment. (not done)");
+                    continue;
+                }
+                try {
+                    imgSeg = result.get();
+                    g.drawImage(imgSeg.getImage(), (int) imgSeg.getBounds().getX(), (int) imgSeg.getBounds().getY(), null);
+                } catch (ExecutionException ex) {
+                    ex.printStackTrace(System.err);
+                    System.err.println("Failed to calculate segment.\n" + ex.getMessage());
+                }
+
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+
+        checkCacheSize();
+        images.put(getCacheKey(), image);
+        panel.setImage(image);
+        panel.repaint();
+
+        hasDrawn = true;
+
+        // Let objects waiting on us know we're done
+        synchronized (this) {
+            notify();
+        }
+    }
+
+    private boolean checkCacheValidity() {
+        if (image == null) return true;
+        return (image.getHeight() == panel.getHeight()) && (image.getWidth() == panel.getWidth());
+    }
+
+    private void calculateVariables() {
         image = panel.createImage();
 
-        // Get an executor service for the amount of cores we have
-        ExecutorService executorService = Executors.newFixedThreadPool(numberThreads);
-
+        iterations = mainWindow.getIterations();
         scaleFactor = mainWindow.getScaleFactor();
         xShift = mainWindow.getTranslateX();
         yShift = mainWindow.getTranslateY();
@@ -69,32 +165,36 @@ class DrawingThread extends Thread {
 
         xScale = xRange / imgWidth;
         yScale = yRange / imgHeight;
+    }
 
-        int iterations = mainWindow.getIterations();
-
-        Line2D bounds;
-        int stripWidth = (int) Math.floor(imgWidth / (numberThreads * 2d));
-        for (int i = 0; i < numberThreads * 2; i++) {
-            int xStart = (i == 0) ? 0 : i * stripWidth;
-            int xEnd = (i == (numberThreads * 2) - 1) ? (int)imgWidth : (i + 1) * stripWidth;
-
-            bounds = new Line2D.Double(xStart, 0, xEnd, imgHeight);
-            executorService.execute(new DiversionCalculator(this, bounds, iterations));
-        }
-
-        executorService.shutdown();
-        while(!executorService.isTerminated()){
-
-        }
-
-        panel.setImage(image);
+    private boolean checkCache() {
+        ImageCache cache = getCacheKey();
+        if (!images.containsKey(cache)) return false;
+        panel.setImage(images.get(cache));
         panel.repaint();
 
-        hasDrawn = true;
+        return true;
+    }
 
-        // Let objects waiting on us know we're done
-        synchronized (this){
-            notify();
+    private ImageCache getCacheKey() {
+        return new ImageCache(iterations, scaleFactor, xShift, yShift);
+    }
+
+    /**
+     * Public method to notify the thread to call redraw
+     */
+    public void draw() {
+        synchronized (this.shouldRedraw) {
+            shouldRedraw.notify();
+        }
+    }
+
+    private void checkCacheSize() {
+        long total = Runtime.getRuntime().totalMemory();
+        long free = Runtime.getRuntime().freeMemory();
+
+        if(free * 100d/ total >= 80){
+            images.remove(images.entrySet().iterator().next().getKey());
         }
     }
 
@@ -107,15 +207,6 @@ class DrawingThread extends Thread {
         y = ((y - imgHeight / 2d) * yScale + yShift) / scaleFactor;
 
         return new Complex(x, y);
-    }
-    public void paintPixel(Point2D p, Color c) {
-        int x = Double.valueOf(p.getX()).intValue();
-        int y = Double.valueOf(p.getY()).intValue();
-
-        // Need to syncronise as we are using multithreading to paint
-        synchronized (image) {
-            image.setRGB(x, y, c.getRGB());
-        }
     }
 
     public boolean hasDrawn(){
