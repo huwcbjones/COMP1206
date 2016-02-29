4f43f4e4ef8f05d2c1741f622542cf317ec97c5a
Huw Jones <huwcbjones@gmail.com>
2016-02-29 02:06:30 +0000

    Added tint option

--- Assignment_1/src/utils/ImagePanel.java
+++ Assignment_1/src/utils/ImagePanel.java
@@ -12,7 +12,9 @@ import java.awt.image.BufferedImage;
  */
 public class ImagePanel extends JPanel {
 
+    private BufferedImage tint;
     private BufferedImage image;
+    private boolean paintTintedImage = false;
 
     public ImagePanel() {
     }
@@ -22,15 +24,45 @@ public class ImagePanel extends JPanel {
 
     public void setImage (BufferedImage image) {
         this.image = image;
+        this.tint = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = this.tint.createGraphics();
+        g.drawImage(image, 0, 0, null);
+        g.dispose();
+
+        paintTintedImage = false;
         repaint();
     }
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.drawImage(image, 0, 0, null);
+        if(paintTintedImage) {
+            g.drawImage(tint, 0, 0, null);
+        } else {
+            g.drawImage(image, 0, 0, null);
+        }
     }
 
     public BufferedImage createImage() {
         return new BufferedImage(this.getWidth(), this.getHeight(), BufferedImage.TYPE_INT_RGB);
+    }
+
+    // TODO: Multi-thread this
+    public void tintImage(double factor) {
+        tintImage(Double.valueOf(factor).floatValue());
+    }
+    public void tintImage(float factor) {
+        if(image == null) return;
+
+        Color c;
+        float[] hsb = new float[3];
+        for(int y = 0; y < image.getHeight(); y++){
+            for(int x = 0; x < image.getWidth(); x++) {
+                c = new Color(image.getRGB(x, y));
+                Color.RGBtoHSB(c.getRed(), c.getBlue(), c.getGreen(), hsb);
+                tint.setRGB(x, y, Color.HSBtoRGB(hsb[0] + factor, hsb[1], hsb[2]));
+            }
+        }
+        paintTintedImage = true;
+        repaint();
     }
 }
