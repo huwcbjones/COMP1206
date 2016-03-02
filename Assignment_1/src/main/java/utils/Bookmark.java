cbb0488244b0c8e66ceb06ab0c13a8dcb01a5e26
Huw Jones <huwcbjones@gmail.com>
2016-03-02 01:19:47 +0000

    Added compareTo, getComplex Changed variable types

--- Assignment_1/src/main/java/utils/Bookmark.java
+++ Assignment_1/src/main/java/utils/Bookmark.java
@@ -6,19 +6,19 @@ package utils;
  * @author Huw Jones
  * @since 01/03/2016
  */
-public class Bookmark {
+public class Bookmark implements Comparable<Bookmark> {
 
     private String name;
-    private float real;
-    private float imaginary;
+    private double real;
+    private double imaginary;
 
-    public Bookmark(String name, float real, float imaginary) {
+    public Bookmark(String name, double real, double imaginary) {
         this.name = name;
         this.real = real;
-        this.imaginary = real;
+        this.imaginary = imaginary;
     }
 
-    public float getImaginary() {
+    public double getImaginary() {
         return imaginary;
     }
 
@@ -26,7 +26,42 @@ public class Bookmark {
         return name;
     }
 
-    public float getReal() {
+    public double getReal() {
         return real;
+    }
+
+    @Override
+    public int compareTo(Bookmark o) {
+        return name.compareTo(o.getName());
+    }
+
+    public Complex getComplex() {
+        return new Complex(real, imaginary);
+    }
+
+    /**
+     * Returns a string representation of the object. In general, the
+     * {@code toString} method returns a string that
+     * "textually represents" this object. The result should
+     * be a concise but informative representation that is easy for a
+     * person to read.
+     * It is recommended that all subclasses override this method.
+     * <p>
+     * The {@code toString} method for class {@code Object}
+     * returns a string consisting of the name of the class of which the
+     * object is an instance, the at-sign character `{@code @}', and
+     * the unsigned hexadecimal representation of the hash code of the
+     * object. In other words, this method returns a string equal to the
+     * value of:
+     * <blockquote>
+     * <pre>
+     * getClass().getName() + '@' + Integer.toHexString(hashCode())
+     * </pre></blockquote>
+     *
+     * @return a string representation of the object.
+     */
+    @Override
+    public String toString() {
+        return String.format("%s (%.5f, %.5f)", name, real, imaginary);
     }
 }
