c4c0dbd26367ac931c25d4d7fd26281c13571845
Huw Jones <huwcbjones@gmail.com>
2016-03-02 01:19:06 +0000

    Added DocumentAdapter

---
+++ Assignment_1/src/main/java/mandelbrot/events/DocumentAdapter.java
@@ -0,0 +1,54 @@
+package mandelbrot.events;
+
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+/**
+ * Implements Adapter for DocumentListener
+ *
+ * @author Huw Jones
+ * @since 02/03/2016
+ */
+public class DocumentAdapter implements DocumentListener {
+    /**
+     * Gives notification that there was an change in the document.
+     *
+     * @param e the document event
+     */
+    public void allUpdate(DocumentEvent e){
+
+    }
+
+    /**
+     * Gives notification that there was an insert into the document.  The
+     * range given by the DocumentEvent bounds the freshly inserted region.
+     *
+     * @param e the document event
+     */
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        allUpdate(e);
+    }
+
+    /**
+     * Gives notification that a portion of the document has been
+     * removed.  The range is given in terms of what the view last
+     * saw (that is, before updating sticky positions).
+     *
+     * @param e the document event
+     */
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        allUpdate(e);
+    }
+
+    /**
+     * Gives notification that an attribute or set of attributes changed.
+     *
+     * @param e the document event
+     */
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        allUpdate(e);
+    }
+}
