db0ffc373ab63981378cd1b68b9b2d0d078c70b5
Huw Jones <huwcbjones@gmail.com>
2016-03-05 16:39:09 +0000

    Commented BookmarkManager.java

--- Assignment_1/src/main/java/mandelbrot/BookmarkManager.java
+++ Assignment_1/src/main/java/mandelbrot/BookmarkManager.java
@@ -34,16 +34,16 @@ import java.util.Map;
  * @since 01/03/2016
  */
 public class BookmarkManager {
+
     private Main mainWindow;
     private ConfigManager config;
-
-    private JPanel panel_bookmarks;
 
     private HashMap<String, Bookmark> bookmarks;
 
     private JList list_bookmarks;
     private DefaultListModel<Bookmark> lm_bookmarks;
 
+    private JPanel panel_bookmarks;
     private JPanel panel_add;
     private JTextField text_name;
     private JButton btn_add;
@@ -65,23 +65,30 @@ public class BookmarkManager {
         init();
     }
 
+    /**
+     * Creates bookmark panel
+     */
     private void init() {
         // Create list model and add elements to it
         lm_bookmarks = new DefaultListModel<>();
+
+        // Add loaded bookmarks to list model
         bookmarks.values().forEach(lm_bookmarks::addElement);
 
+        // Create list view
         list_bookmarks = new JList(lm_bookmarks);
         list_bookmarks.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         list_bookmarks.setLayoutOrientation(JList.VERTICAL);
         list_bookmarks.addListSelectionListener(new bookmarkSelectionHandler());
 
+        // Create scroller to scroll list view if there are too many bookmarks
         JScrollPane listScroller = new JScrollPane(list_bookmarks);
         panel_bookmarks.add(listScroller, BorderLayout.CENTER);
 
+        // Add add panel at the bottom of the border layouts
         panel_add = new JPanel(new GridBagLayout());
         panel_add.setBorder(new EmptyBorder(6, 0, 6, 0));
         panel_bookmarks.add(panel_add, BorderLayout.PAGE_END);
-
 
         text_name = new JTextField();
         text_name.setUI(new HintTextFieldUI("Bookmark Name", true, Color.lightGray));
@@ -91,7 +98,6 @@ public class BookmarkManager {
         btn_add.setMnemonic(KeyEvent.VK_A);
         btn_add.setEnabled(false);
         btn_add.addActionListener(new addClickHandler());
-
 
         btn_delete = new JButton("Delete");
         btn_delete.setMnemonic(KeyEvent.VK_D);
@@ -114,13 +120,16 @@ public class BookmarkManager {
         panel_add.add(btn_delete, constraints);
     }
 
-
     //region Saving/Loading
 
+    /**
+     * Loads the bookmarks from file to memory
+     */
     private void loadBookmarks() {
         Log.Information("Loading bookmarks...");
         bookmarks = new HashMap<>();
 
+        // Get bookmarks.json from resources
         InputStream inputStream = this.getClass().getResourceAsStream("/mandelbrot/bookmarks.json");
         if (inputStream == null) {
             Log.Warning("Failed to find bookmarks.json!");
@@ -130,11 +139,15 @@ public class BookmarkManager {
         JSONParser parser = new JSONParser();
 
         try {
+            // Parse JSON file
             JSONObject root = (JSONObject) parser.parse(reader);
             JSONArray bookmarkArray = (JSONArray) root.get("bookmarks");
 
             Iterator jsonIterator = bookmarkArray.iterator();
             JSONObject bookmark;
+
+            // Loop over bookmarks and create their Bookmark object
+            // Then add then to the HashMap
             while (jsonIterator.hasNext()) {
                 bookmark = (JSONObject) jsonIterator.next();
                 String name = (String) bookmark.get("name");
@@ -151,17 +164,20 @@ public class BookmarkManager {
         }
     }
 
-
-    //TODO: Save bookmarks to JSON file
+    /**
+     * Saves the bookmarks from memory to file
+     */
     public void saveBookmarks() {
         Log.Information("Saving bookmarks...");
         try {
             PrintWriter writer = new PrintWriter(new File(this.getClass().getResource("/mandelbrot/bookmarks.json").getPath()));
 
+            // Create root object
             JSONObject bookmarks = new JSONObject();
             JSONObject bookmark;
             JSONArray bookmarksArray = new JSONArray();
 
+            // Create bookmark JSON Object and add them to the array
             Bookmark bookmarkObj;
             for(Map.Entry<String, Bookmark> b: this.bookmarks.entrySet()){
                 bookmarkObj = b.getValue();
@@ -172,12 +188,15 @@ public class BookmarkManager {
                 bookmarksArray.add(bookmark);
             }
 
+            // Add the array to the root object
             bookmarks.put("bookmarks", bookmarksArray);
+
+            // Write out the new file
             StringWriter out = new StringWriter();
             bookmarks.writeJSONString(out);
-
             writer.write(out.toString());
             writer.close();
+
         } catch (FileNotFoundException e) {
             Log.Warning("Could not find file to write to!");
         } catch (IOException e) {
@@ -191,18 +210,31 @@ public class BookmarkManager {
         return this.panel_bookmarks;
     }
 
+    /**
+     * Adds a bookmark object to the HashMap
+     * @param bookmark bookmark to add
+     */
     public void addBookmark(Bookmark bookmark) {
         this.bookmarks.put(bookmark.getName(), bookmark);
     }
 
     //region Event Handling
     private void setBtnAddState() {
+        // If no text is entered, can't save
         boolean btn_enabled = !text_name.getText().equals("");
+
+        // If name already exists, can't save
         btn_enabled &= !bookmarks.containsKey(text_name.getText());
+
+        // If selected point is null, can't save
         btn_enabled &= config.getSelectedPoint() != null;
+
         btn_add.setEnabled(btn_enabled);
     }
 
+    /**
+     * Updates the btn_add state when the text field content changes
+     */
     private class textChangeHandler extends DocumentAdapter {
 
         /**
@@ -216,6 +248,9 @@ public class BookmarkManager {
         }
     }
 
+    /**
+     * Updates the btn_add state when the selected point changes
+     */
     private class selectedPointHandler extends ConfigChangeAdapter {
         @Override
         public void selectedPointChange(Complex complex) {
@@ -223,6 +258,9 @@ public class BookmarkManager {
         }
     }
 
+    /**
+     * Updates the selected point when the selected bookmark changes
+     */
     private class bookmarkSelectionHandler implements ListSelectionListener {
 
         /**
@@ -241,38 +279,45 @@ public class BookmarkManager {
         }
     }
 
+    /**
+     * Invoked when btn_add is clicked
+     */
     private class addClickHandler implements ActionListener {
 
-        /**
-         * Invoked when an action occurs.
-         *
-         * @param e
-         */
         @Override
         public void actionPerformed(ActionEvent e) {
+            // Create new bookmark
             Bookmark bookmark = new Bookmark(text_name.getText(), config.getSelectedPoint());
+
+            // Add it to HashMap
             addBookmark(bookmark);
+
+            // Add it to ListView
             lm_bookmarks.addElement(bookmark);
+
+            // Clear text field
             text_name.setText("");
+
             saveBookmarks();
         }
     }
 
+    /**
+     * Invoked when btn_delete is clicked
+     */
     private class deleteClickHandler implements ActionListener {
 
-        /**
-         * Invoked when an action occurs.
-         *
-         * @param e
-         */
         @Override
         public void actionPerformed(ActionEvent e) {
+            // Check if a bookmark is selected, otherwise return
             List selectedObj = list_bookmarks.getSelectedValuesList();
             btn_delete.setEnabled(selectedObj.size() == 1);
             if (selectedObj.size() != 1) return;
 
+            // Get selected bookmark
             Bookmark bookmark = (Bookmark) selectedObj.get(0);
 
+            // Prompt user if they want to delete it
             int option = JOptionPane.showConfirmDialog(
                     mainWindow,
                     "Are you sure want to delete the bookmark\n" +
@@ -280,6 +325,8 @@ public class BookmarkManager {
                     "Delete Bookmark",
                     JOptionPane.YES_NO_OPTION
             );
+
+            // If they do, then remove it
             if(option == JOptionPane.YES_OPTION) {
                 lm_bookmarks.removeElement(bookmark);
                 bookmarks.remove(bookmark.getName());
