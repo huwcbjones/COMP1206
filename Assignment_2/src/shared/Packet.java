bc735da39fa20f545feea5afcc46fcb42bbc2273
Huw Jones <huwcbjones@gmail.com>
2016-03-26 16:48:00 +0000

    Added Packet and PacketType

---
+++ Assignment_2/src/shared/Packet.java
@@ -0,0 +1,30 @@
+package shared;
+
+import java.io.Serializable;
+
+/**
+ * Represents a packet of object type, T extends Serializable, sent between client and server
+ *
+ * @author Huw Jones
+ * @since 26/03/2016
+ */
+public class Packet<T extends Serializable> implements Serializable {
+
+    public static final long serialUID = 1L;
+
+    private final PacketType type;
+    private final T payload;
+
+    public Packet (PacketType type, T payload) {
+        this.type = type;
+        this.payload = payload;
+    }
+
+    public T getPayload () {
+        return this.payload;
+    }
+
+    public PacketType getType () {
+        return this.type;
+    }
+}
---
+++ Assignment_2/src/shared/PacketType.java
@@ -0,0 +1,11 @@
+package shared;
+
+/**
+ * Defines a type of message
+ *
+ * @author Huw Jones
+ * @since 26/03/2016
+ */
+public enum PacketType {
+
+}
