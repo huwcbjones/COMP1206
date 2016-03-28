e04bcef4e1e4dd5d5ecb68921dd538bdfef13a65
Huw Jones <huwcbjones@gmail.com>
2016-03-28 16:41:35 +0100

    Added LoginEventListener

---
+++ Assignment_2/src/main/java/client/events/LoginEventListener.java
@@ -0,0 +1,26 @@
+package client.events;
+
+import shared.User;
+
+/**
+ * Handles Login Events
+ *
+ * @author Huw Jones
+ * @since 28/03/2016
+ */
+public interface LoginEventListener {
+
+    /**
+     * Fired when a successful login occurs
+     *
+     * @param user User object for current user
+     */
+    void loginSuccess (User user);
+
+    /**
+     * Fire when an unsuccessful login occurs
+     *
+     * @param message Reason why login failed
+     */
+    void loginError (String message);
+}
