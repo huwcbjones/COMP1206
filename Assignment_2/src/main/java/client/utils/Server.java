5183677f89a2a7999527bc38074d49744c27b9c8
Huw Jones <huwcbjones@gmail.com>
2016-03-28 20:13:38 +0100

    Removed secure port... Connection should automatically be upgraded if the server supports it

--- Assignment_2/src/main/java/client/utils/Server.java
+++ Assignment_2/src/main/java/client/utils/Server.java
@@ -11,23 +11,16 @@ public final class Server {
     private final String name;
     private final String address;
     private final int port;
-    private final boolean useSecurePort;
-    private final int securePort;
 
 
-    public Server (String address, int port, boolean useSecurePort, int securePort) {
-        this(
-                // this() has to be first statement... let's make us a server name!
-                address + " (" + ( ( useSecurePort ) ? securePort : port ) + ")",
-                address, port, useSecurePort, securePort);
+    public Server (String address, int port) {
+        this(address + "(" + port + ")", address, port);
     }
 
-    public Server (String name, String address, int port, boolean useSecurePort, int securePort) {
+    public Server (String name, String address, int port) {
         this.name = name;
         this.address = address;
         this.port = port;
-        this.useSecurePort = useSecurePort;
-        this.securePort = securePort;
     }
 
     /**
@@ -57,28 +50,8 @@ public final class Server {
         return port;
     }
 
-    /**
-     * Returns whether or not we should try to use the server's secure connection
-     *
-     * @return true if we should use the secure connection
-     */
-    public boolean useSecurePort () {
-        return useSecurePort;
-    }
-
-    /**
-     * Returns the secure port the server is listening on
-     *
-     * @return int, secure port
-     */
-    public int getSecurePort () {
-        return securePort;
-    }
-
     @Override
     public String toString () {
-        String toString = this.name;
-        if (this.useSecurePort) toString += " *";
-        return toString;
+        return this.name;
     }
 }
