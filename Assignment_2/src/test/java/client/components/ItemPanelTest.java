edb8de105a7a9fa426f9ea84a11919d97e8e6485
Huw Jones <huwcbjones@gmail.com>
2016-04-20 19:37:55 +0100

    Added ItemPanel Test

---
+++ Assignment_2/src/test/java/client/components/ItemPanelTest.java
@@ -0,0 +1,49 @@
+package client.components;
+
+import org.junit.Test;
+import shared.Item;
+import shared.ItemBuilder;
+
+import javax.swing.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.UUID;
+
+
+/**
+ * ItemPanelTest
+ *
+ * @author Huw Jones
+ * @since 20/04/2016
+ */
+public class ItemPanelTest {
+
+    public static void main(String[] args){
+        ItemPanelTest t = new ItemPanelTest();
+        t.ItemPanelTest();
+    }
+    @Test
+    public void ItemPanelTest(){
+        Calendar endTime = Calendar.getInstance();
+        endTime.add(Calendar.DAY_OF_WEEK, 14);
+
+        ItemBuilder itemBuilder = Item.createBuilder();
+        itemBuilder
+            .setID(UUID.randomUUID())
+            .setUserID(UUID.randomUUID())
+            .setTitle("Test Item")
+            .setDescription("Brand new Test Item.<br /> Never opened before.<br /> Pristine condition.")
+            .setReservePrice(new BigDecimal("2.50"))
+            .setEndTime(new Timestamp(endTime.getTime().getTime()))
+            .addKeyword("test");
+
+        JFrame frame = new JFrame("ItemPanel Test");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        ItemPanel panel = new ItemPanel(itemBuilder.getItem());
+        frame.add(panel);
+        //frame.setMinimumSize(new Dimension(560, 212));
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
