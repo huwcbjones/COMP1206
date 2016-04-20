0e3bc451cd0432311df85e0c9711cc703e72c778
Huw Jones <huwcbjones@gmail.com>
2016-04-20 12:17:11 +0100

    Added menubar to Main

--- Assignment_2/src/main/java/client/windows/Main.java
+++ Assignment_2/src/main/java/client/windows/Main.java
@@ -13,8 +13,15 @@ import java.awt.*;
  */
 public final class Main extends WindowTemplate {
 
+    private JMenuBar menuBar;
+    private JMenu menu_file;
+    private JMenu menu_items;
+    private JMenu menu_options;
+    private JMenu menu_help;
+
+
     public Main() {
-        super("View Items");
+        super("Home");
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setMinimumSize(new Dimension(800, 600));
 
@@ -25,6 +32,32 @@ public final class Main extends WindowTemplate {
      */
     @Override
     protected void initComponents() {
+        this.initMainMenu();
+    }
 
+    private void initMainMenu(){
+        this.menuBar = new JMenuBar();
+
+        this.menu_file = new JMenu("File");
+        this.menu_file.setMnemonic('f');
+
+        this.menuBar.add(this.menu_file);
+
+        this.menu_items = new JMenu("Items");
+        this.menu_items.setMnemonic('i');
+
+        this.menuBar.add(this.menu_items);
+
+        this.menu_options = new JMenu("Options");
+        this.menu_options.setMnemonic('o');
+
+        this.menuBar.add(this.menu_options);
+
+        this.menu_help = new JMenu("Help");
+        this.menu_help.setMnemonic('h');
+
+        this.menuBar.add(this.menu_help);
+
+        this.setJMenuBar(this.menuBar);
     }
 }
