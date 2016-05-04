af78b4a906c7d72ae41d942e7de6da831ea5da38
Huw Jones <huwcbjones@gmail.com>
2016-05-04 01:54:28 +0100

    Added TextArea hint UI

---
+++ Assignment_2/src/main/java/shared/components/HintTextAreaUI.java
@@ -0,0 +1,109 @@
+package shared.components;
+
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicTextAreaUI;
+import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+/**
+ * TextFieldUI, with input hint (like HTML text inputs)
+ * Taken from: http://stackoverflow.com/a/4962829/5909019
+ *
+ * @author Huw Jones
+ * @since 01/03/2016
+ */
+public class HintTextAreaUI extends BasicTextAreaUI implements FocusListener {
+
+    private String hint;
+    private boolean hideOnFocus;
+    private Color color;
+
+    public HintTextAreaUI(){
+        this("");
+    }
+    public HintTextAreaUI(String hint) {
+        this(hint, false);
+    }
+
+    public HintTextAreaUI(String hint, boolean hideOnFocus) {
+        this(hint, hideOnFocus, Color.lightGray);
+    }
+
+    public HintTextAreaUI(String hint, boolean hideOnFocus, Color color) {
+        this.hint = hint;
+        this.hideOnFocus = hideOnFocus;
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+        this.repaint();
+    }
+
+    private void repaint() {
+        if (this.getComponent() != null) {
+            this.getComponent().repaint();
+        }
+    }
+
+    public boolean isHideOnFocus() {
+        return this.hideOnFocus;
+    }
+
+    public void setHideOnFocus(boolean hideOnFocus) {
+        this.hideOnFocus = hideOnFocus;
+        this.repaint();
+    }
+
+    public String getHint() {
+        return this.hint;
+    }
+
+    public void setHint(String hint) {
+        this.hint = hint;
+        this.repaint();
+    }
+
+    @Override
+    public void focusGained(FocusEvent e) {
+        if (this.hideOnFocus) this.repaint();
+
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        if (this.hideOnFocus) this.repaint();
+    }
+
+    @Override
+    protected void installListeners() {
+        super.installListeners();
+        this.getComponent().addFocusListener(this);
+    }
+
+    @Override
+    protected void uninstallListeners() {
+        super.uninstallListeners();
+        this.getComponent().removeFocusListener(this);
+    }
+
+    @Override
+    protected void paintSafely(Graphics g) {
+        super.paintSafely(g);
+        JTextArea comp = (JTextArea)this.getComponent();
+        if (this.hint != null && comp.getText().length() == 0 && !(this.hideOnFocus && comp.hasFocus())) {
+            if (this.color != null) {
+                g.setColor(this.color);
+            } else {
+                g.setColor(comp.getForeground().brighter().brighter().brighter());
+            }
+            int padding = ((comp.getHeight() / comp.getRows()) - comp.getFont().getSize());
+            g.drawString(this.hint, 3, comp.getHeight() - (comp.getRows() * comp.getFont().getSize()) - padding);
+        }
+    }
+}
