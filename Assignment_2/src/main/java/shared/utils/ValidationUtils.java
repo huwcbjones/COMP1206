fb78a5a6947af258ce2bfbb4be0ce9dd56eb84e5
Huw Jones <huwcbjones@gmail.com>
2016-05-09 17:52:23 +0100

    Added Title/Description validation

--- Assignment_2/src/main/java/shared/utils/ValidationUtils.java
+++ Assignment_2/src/main/java/shared/utils/ValidationUtils.java
@@ -21,8 +21,9 @@ public final class ValidationUtils {
     public static final int VALIDATION_CLEAR = 0;
     public static final int VALIDATION_SUCCESS = 1;
     public static final int VALIDATION_FAIL = 2;
-    public static final String REGEXP_USERNAME = "[\\w]*";
-    public static final String REGEXP_NAME = "^[a-zA-Z]+[a-zA-Z\\-]*$";
+    public static final String REGEXP_USERNAME = "^[a-zA-Z0-9_]{3,64}$";
+    public static final String REGEXP_NAME = "^[a-zA-Z][a-zA-Z\\-]{2,64}$";
+    public static final String REGEXP_TITLE = "^[a-zA-Z0-9\\- ;:,.]{3,140}$";
 
     private static final Border BORDER_DEFAULT = UIManager.getBorder("TextField.border");
     private static final Border BORDER_SUCCESS = new CompoundBorder(BorderFactory.createLineBorder(new Color(0, 128, 0), 1), new EmptyBorder(2,2,2,2));
@@ -128,22 +129,21 @@ public final class ValidationUtils {
             }
         }
 
-        if (lower && upper && number) {
-            return;
-        }
         throw new ValidationFailedException("Password must contain lower case, upper case and a number, or be longer than 32 characters.");
     }
 
     public static void validateUsername(String username) throws ValidationFailedException {
         validateUsername(username, true);
     }
-
     public static void validateUsername(String username, boolean isRequired) throws ValidationFailedException {
         if(username.equals("") && isRequired){
             throw new ValidationFailedException("Username is required");
         }
         if (username.length() < 3) {
             throw new ValidationFailedException("Usernames must be longer than 3 characters.");
+        }
+        if (username.length() > 64) {
+            throw new ValidationFailedException("Username cannot be longer than 64 characters.");
         }
         if (username.matches(REGEXP_USERNAME)) {
             return;
@@ -161,11 +161,44 @@ public final class ValidationUtils {
         if (name.length() < 3) {
             throw new ValidationFailedException("Names must be longer than 3 characters.");
         }
+        if (name.length() > 64) {
+            throw new ValidationFailedException("Names cannot be longer than 64 characters.");
+        }
         if (name.matches(REGEXP_NAME)) {
             return;
         }
         throw new ValidationFailedException("Names can only contain letters and hyphens (-).");
     }
 
+    public static void validateTitle(String title) throws ValidationFailedException {
+        validateTitle(title, true);
+    }
+    public static void validateTitle(String title, boolean isRequired) throws ValidationFailedException {
+        if(title.equals("") && isRequired){
+            throw new ValidationFailedException("Title is required");
+        }
+        if (title.length() < 3) {
+            throw new ValidationFailedException("Title must be longer than 3 characters.");
+        }
+        if (title.length() > 140) {
+            throw new ValidationFailedException("Title has a maximum length of 140 characters.");
+        }
+        if (title.matches(REGEXP_TITLE)) {
+            return;
+        }
+        throw new ValidationFailedException("Titles can only contain alphanumeric characters, and some punctuation (-, ;, :, ., ,).");
+    }
+
+    public static void validateDescription(String title) throws ValidationFailedException {
+        validateDescription(title, true);
+    }
+    public static void validateDescription(String title, boolean isRequired) throws ValidationFailedException {
+        if(title.equals("") && isRequired){
+            throw new ValidationFailedException("Description is required");
+        }
+        if (title.length() < 3) {
+            throw new ValidationFailedException("Description must be longer than 3 characters.");
+        }
+    }
     // public static void
 }
