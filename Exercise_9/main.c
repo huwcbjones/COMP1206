a0abef76c74a998d8b539b1f7293690d36f32cfa
Huw Jones <huwcbjones@gmail.com>
2016-05-13 12:00:30 +0100

    Fixed main.c?

--- Exercise_9/main.c
+++ Exercise_9/main.c
@@ -2,11 +2,11 @@
 #include "analyse.c"
 
 int main () {
-    double number = 15d;
+    double number = 15;
     int sign;
-    int whole;
-    double fraction;
+    int *whole;
+    double *fraction;
 
-    sign = analyse(*whole, *fraction, number);
+    sign = analyse(whole, fraction, number);
     printf("Analysis of: %f, (sign: %i, whole: %i, fraction: %f)", number, sign, whole, fraction);
 }
