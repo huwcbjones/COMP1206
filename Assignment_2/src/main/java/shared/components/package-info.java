8ecad7de88c2d2c12e331b31d872ce3bcc388ecc
Huw Jones <huwcbjones@gmail.com>
2016-05-13 15:50:21 +0100

    Commented shared.components and shared.events

---
+++ Assignment_2/src/main/java/shared/components/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Components used by both client and server GUIs
+ *
+ * @author Huw Jones
+ * @since 13/05/2016
+ */
+package shared.components;
---
+++ Assignment_2/src/main/java/shared/events/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Shared events
+ *
+ * @author Huw Jones
+ * @since 13/05/2016
+ */
+package shared.events;
